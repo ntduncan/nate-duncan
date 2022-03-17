1a51cb8f6e607ec65285cb90791e09a2db00c6f2
ntduncan <nateduncan0@.com>
2022-03-16 18:41:07 -0600

    added page break animation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import PageOne from './pages/PageOne'
+import PageOne from './pages/PageOne';
+import WaveBlock from './pages/WaveBlock/WaveBlock'
 
 function App() {
   return (
     <div className="App">
       <PageOne />
+      <WaveBlock />
     </div>
   );
 }
