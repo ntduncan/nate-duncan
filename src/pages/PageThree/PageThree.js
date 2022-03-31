dcd0fd044338935821b9020cee249f2731130944
ntduncan <nateduncan0@.com>
2022-03-31 09:50:44 -0600

    updated blog cards

--- src/pages/PageThree/PageThree.js
+++ src/pages/PageThree/PageThree.js
@@ -10,37 +10,35 @@ export default function PageThree() {
         <WaveBlock />
       </div>
       <div className="about-me">
-        <div className="about-me__right">
+        <h1 className="about-me__title">When I'm not working I am...</h1>
+        <div className="about-me__body">
           <BlogCardLeft alternate={false}>
-            <h1>Learning to Code</h1>
-            <h2>Opening a door to the future</h2>
+            <h1>Fly Fishing</h1>
+            <h2>Getting outside</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum
-              dolorum architecto obcaecati enim dicta praesentium, quam nobis!
-              Neque ad aliquam facilis numquam. Veritatis, sit.
+              Coding is great, but it is can easily become your career and hobby. I like fly fishing
+              because it is a good excuse to get away from the computer and get some fresh air.
             </p>
           </BlogCardLeft>
           <BlogCardLeft alternate={true}>
             <h1>Learning to Code</h1>
             <h2>Opening a door to the future</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum
-              dolorum architecto obcaecati enim dicta praesentium, quam nobis!
-              Neque ad aliquam facilis numquam. Veritatis, sit.
+              I love coding! More than just coding, I enjoy learning to code. I spend a good amount of 
+              my free time taking Udemy courses, exploring codepen.io, and testing out new technologies
+              that I haven't tried out yet.
             </p>
           </BlogCardLeft>
           <BlogCardLeft alternate={false}>
-            <h1>Learning to Code</h1>
-            <h2>Opening a door to the future</h2>
+            <h1>Reading</h1>
+            <h2>Experience life in a differnt time</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum
-              dolorum architecto obcaecati enim dicta praesentium, quam nobis!
-              Neque ad aliquam facilis numquam. Veritatis, sit.
+              I enjoy deep thinking into philisophical topics. Old literature is a great source of
+              deep thinking opportunities. Reading books from 50 - 100 years ago allows the reader to see life
+              through a completely differnt lense.
             </p>
           </BlogCardLeft>
         </div>
-        <div className="about-me__left"></div>
-        <div className="about-me__right"></div>
       </div>
     </div>
   );
